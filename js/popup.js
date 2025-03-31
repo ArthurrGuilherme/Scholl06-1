0c258201c07d68f7f752451ba002b1a6dfa8c3e7
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2025-03-31 04:44:47 -0300

    Add files via upload

--- js/popup.js
+++ js/popup.js
@@ -1,36 +1,40 @@
-function mostrarPopup(id) {
-    document.getElementById(id).style.display = "flex";
-    document.getElementById(id).setAttribute("aria-hidden", "false");
-}
+/*Container03*/
+    /*Popup*/
+        function mostrarPopup(id) {
+            document.getElementById(id).style.display = "flex";
+            document.getElementById(id).setAttribute("aria-hidden", "false");
+        }
 
-function fecharPopup(id) {
-    document.getElementById(id).style.display = "none";
-    document.getElementById(id).setAttribute("aria-hidden", "true");
-}
+        function fecharPopup(id) {
+            document.getElementById(id).style.display = "none";
+            document.getElementById(id).setAttribute("aria-hidden", "true");
+        }
 
-function checkInput(inputId, btnContainerId) {
-    const inputText = document.getElementById(inputId).value;
-    const btnContainer = document.getElementById(btnContainerId);
-    if (inputText.trim() !== "") {
-        btnContainer.style.visibility = "visible";
-        btnContainer.style.display = "flex"; // Certifica que ele aparece
-    } else {
-        btnContainer.style.visibility = "hidden";
-        btnContainer.style.display = "none"; // Oculta corretamente
-    }
-}
+        function checkInput(inputId, btnContainerId) {
+            const inputText = document.getElementById(inputId).value;
+            const btnContainer = document.getElementById(btnContainerId);
+            if (inputText.trim() !== "") {
+                btnContainer.style.visibility = "visible";
+                btnContainer.style.display = "flex"; // Certifica que ele aparece
+            } else {
+                btnContainer.style.visibility = "hidden";
+                btnContainer.style.display = "none"; // Oculta corretamente
+            }
+        }
 
-function enviarTexto(inputId, popupId, mensagem) {
-    const inputValue = document.getElementById(inputId).value;
-    if (!inputValue.trim()) {
-        Swal.fire('Erro', 'Por favor, preencha o campo!', 'error');
-        return;
-    }
+        function enviarTexto(inputId, popupId, mensagem) {
+            const inputValue = document.getElementById(inputId).value;
+            if (!inputValue.trim()) {
+                Swal.fire('Erro', 'Por favor, preencha o campo!', 'error');
+                return;
+            }
 
-    Swal.fire({
-        title: 'Exemplo de resposta <br><br>',
-        html: `<p>${mensagem}</p>`,
-        icon: 'success',
-        confirmButtonText: 'Fechar'
-    }).then(() => fecharPopup(popupId));
-}
+            Swal.fire({
+                title: 'Exemplo de resposta',
+                html: `<p>${mensagem}</p>`,
+                icon: 'success',
+                confirmButtonText: 'Fechar'
+            }).then(() => fecharPopup(popupId));
+        }
+    /*Popup*/
+/*Container03*/
